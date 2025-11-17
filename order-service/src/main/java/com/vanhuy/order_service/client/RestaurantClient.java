df409058f4a08bea6c19e09a9c21b9158668c178
tcwiuy <tangcamminh2000@gmail.com>
2025-11-18 00:12:06 +0700

    Fix CI/CD with Multi-stage Docker builds

--- order-service/src/main/java/com/vanhuy/order_service/client/RestaurantClient.java
+++ order-service/src/main/java/com/vanhuy/order_service/client/RestaurantClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.math.BigDecimal;
 
-@FeignClient(name = "restaurant-service", url = "http://localhost:8082")
+@FeignClient(name = "restaurant-service")
 public interface RestaurantClient {
     @GetMapping("/api/v1/menu-items/{menuItemId}")
     BigDecimal getPriceByMenuItemId(@PathVariable Integer menuItemId);

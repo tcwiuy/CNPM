9e8bb7dc07318147b3bd185ea06ddd2293607b6e
Kỳ Vỹ <kyvy.2004@gmail.com>
2025-11-18 09:20:54 +0700

    restore ci cd

--- notification-service/src/test/java/com/vanhuy/notification_service/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/com/vanhuy/notification_service/NotificationServiceApplicationTests.java
@@ -2,13 +2,12 @@ package com.vanhuy.notification_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles; // Import cần thiết
 
 @SpringBootTest
-@ActiveProfiles("test-ci") // <-- THÊM DÒNG NÀY
 class NotificationServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
+
 }

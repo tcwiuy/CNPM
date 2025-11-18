e90748f239596575092a62ce879e411003bcec4e
Kỳ Vỹ <kyvy.2004@gmail.com>
2025-11-18 09:18:13 +0700

    update notification-service-test

--- notification-service/src/test/java/com/vanhuy/notification_service/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/com/vanhuy/notification_service/NotificationServiceApplicationTests.java
@@ -2,12 +2,13 @@ package com.vanhuy.notification_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles; // Import cần thiết
 
 @SpringBootTest
+@ActiveProfiles("test-ci") // <-- THÊM DÒNG NÀY
 class NotificationServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }

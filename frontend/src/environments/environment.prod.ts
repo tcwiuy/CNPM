3924cad1f1890797610e21cded16103b20ccc61f
tcwiuy <tangcamminh2000@gmail.com>
2025-11-18 13:40:53 +0700

    Fix(frontend): Update API base URL to public IP

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    baseUrl: '20.205.6.4:9000/api/v1' // Change this to your production URL when deploying
+    baseUrl: 'http://20.205.6.4:9000/api/v1' // Change this to your production URL when deploying
   };

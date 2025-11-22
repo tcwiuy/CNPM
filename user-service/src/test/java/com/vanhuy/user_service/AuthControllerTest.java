31ed44fc0d7de22bb4ecfc7f9f8d6013c13ccbd3
Kỳ Vỹ <kyvy.2004@gmail.com>
2025-11-23 03:00:23 +0700

    update unit test AuthControllerTest

--- user-service/src/test/java/com/vanhuy/user_service/AuthControllerTest.java
+++ user-service/src/test/java/com/vanhuy/user_service/AuthControllerTest.java
@@ -9,6 +9,7 @@ import com.vanhuy.user_service.service.PasswordResetService;
 import com.vanhuy.user_service.component.JwtUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AuthController.class)
+@AutoConfigureMockMvc(addFilters = false) // Bỏ qua security
 public class AuthControllerTest {
 
     @Autowired
@@ -51,4 +53,5 @@ public class AuthControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.message").value("User registered successfully"));
     }
+
 }

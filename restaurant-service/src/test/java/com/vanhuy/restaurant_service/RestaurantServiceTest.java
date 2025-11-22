7cae0b9a19df1411a2dc713e3f5ad3a3472c3c5a
Kỳ Vỹ <kyvy.2004@gmail.com>
2025-11-23 01:01:14 +0700

    config workflow for test and update RestaurantTestService

--- restaurant-service/src/test/java/com/vanhuy/restaurant_service/RestaurantServiceTest.java
+++ restaurant-service/src/test/java/com/vanhuy/restaurant_service/RestaurantServiceTest.java
@@ -3,7 +3,7 @@ package com.vanhuy.restaurant_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class RestaurantServiceApplicationTests {
 
     @Test

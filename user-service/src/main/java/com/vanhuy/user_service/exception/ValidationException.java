a431dc47561415c8b775981b13186e332565119c
Kỳ Vỹ <kyvy.2004@gmail.com>
2025-11-25 06:37:18 +0700

    final

--- user-service/src/main/java/com/vanhuy/user_service/exception/ValidationException.java
+++
@@ -1,8 +0,0 @@
-package com.vanhuy.user_service.exception;
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
-
